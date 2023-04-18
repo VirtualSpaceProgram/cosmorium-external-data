f8ae3e859c59de917123ad6964efe15fc1d56d3d
100の人 <100@pokemori.jp>
2023-04-18 18:15:44 +0900

    [GitHub Actions] CelesTrakから1時間ごとに人工衛星の位置データを取得して Pages の data.txt へ出力

---
+++ fetch-data.js
@@ -0,0 +1,13 @@
+import fs from 'node:fs/promises';
+import yaml from 'js-yaml';
+
+const PAGES_DIRECTORY_PATH = './_site/';
+
+const data = [ ];
+for (const url of yaml.load(await fs.readFile(new URL('satellites.yaml', import.meta.url), { encoding: 'utf-8' }))) {
+	data.push(await (await fetch(url)).text());
+}
+
+const pagesDirectory = new URL(PAGES_DIRECTORY_PATH, import.meta.url);
+await fs.mkdir(pagesDirectory, { recursive: true });
+await fs.writeFile(new URL('data.txt', pagesDirectory), data.join(''));
