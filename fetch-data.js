7c3c280152f7722d1bf65b600fb391a3ba16d5b1
100の人 <100@pokemori.jp>
2023-04-20 21:15:52 +0900

    [衛星の位置] リポジトリ名変更にともない、出力先を「data.txt」から「satellites.txt」に

--- fetch-data.js
+++ fetch-data.js
@@ -10,4 +10,4 @@ for (const url of yaml.load(await fs.readFile(new URL('satellites.yaml', import.
 
 const pagesDirectory = new URL(PAGES_DIRECTORY_PATH, import.meta.url);
 await fs.mkdir(pagesDirectory, { recursive: true });
-await fs.writeFile(new URL('data.txt', pagesDirectory), data.join(''));
+await fs.writeFile(new URL('satellites.txt', pagesDirectory), data.join(''));
