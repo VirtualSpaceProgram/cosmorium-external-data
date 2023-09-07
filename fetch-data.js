1e439007c7ef5190b14d0b4fe9d7a156deb843ff
100の人 <100@pokemori.jp>
2023-09-07 19:58:42 +0900

    [衛星の位置] 改行を含まないデータを無視するようにした (データが存在しないカタログ番号を指定していた場合)
    データが存在しないカタログ番号を参照していた場合、200 OKで「No GP data found」が返る。

--- fetch-data.js
+++ fetch-data.js
@@ -1,10 +1,17 @@
 import fs from 'node:fs/promises';
 import yaml from 'js-yaml';
+import * as core from '@actions/core';
 
 const PAGES_DIRECTORY_PATH = './_site/';
 
 const data = [ ];
 for (const url of yaml.load(await fs.readFile(new URL('satellites.yaml', import.meta.url), { encoding: 'utf-8' }))) {
+	const responseText = await (await fetch(url)).text();
+	if (!responseText.includes('\n')) {
+		// 改行が含まれていなければ、404とみなす
+		core.error(`${url} から「${responseText}」が返りました。`);
+		continue;
+	}
 	data.push(await (await fetch(url)).text());
 }
 
